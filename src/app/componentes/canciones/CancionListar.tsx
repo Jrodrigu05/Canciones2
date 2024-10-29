cda19392b59da3acd4c18f685b744011ea655f7b
scaceres5 <lcastro76@cuc.edu.co>
2024-10-29 13:16:08 -0500

    Administrar con iconos

--- src/app/componentes/canciones/CancionListar.tsx
+++ src/app/componentes/canciones/CancionListar.tsx
@@ -1,8 +1,53 @@
 export const CancionListar = () => {
     return (
       <>
-        Cancion listar <br />
-        Canciones...
+      <div className="pt-5 d-flex justify-content-center">
+        <div className="col-md-8">
+        <table className="table table-bordered border-primary">
+  <thead>
+    <tr>
+      <td>No.</td>
+      <th>Titulo Cancion</th>
+      <th>Cantante</th>
+      <th>Duración</th>
+      <th>Genero</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>La noche</td>
+      <td>Joe arroyo</td>
+      <td>4.5</td>
+      <td>Salsa</td>
+    </tr>
+    <tr>
+    <td>2</td>
+      <td>Mi ex tenia razón</td>
+      <td>Karol G</td>
+      <td>4.5</td>
+      <td>Regueton</td>
+    </tr>
+    <tr>
+    <td>3</td>
+      <td>La Sin medir distancia</td>
+      <td>Diomedes Diaz</td>
+      <td>4.5</td>
+      <td>Vallenato</td>
+    </tr>
+    <tr>
+    <td>3</td>
+      <td>My inmortal</td>
+      <td>Evanescense</td>
+      <td>4.5</td>
+      <td>Metal soft</td>
+    </tr>
+    
+  </tbody>
+</table>
+</div>
+</div>
+
       </>
     );
   };
